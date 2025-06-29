45d3b7609637925bb2ce743b324073651d10174b
Julia Llama <julia.persidskaia@gmail.com>
2025-06-29 22:32:33 +0300

    Add the solution to exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,26 +10,38 @@ const Header = (props) => {
 
 const FeedbackRes = ({good, neutral, bad}) => {
   // console.log("Counter:", {good, neutral, bad})
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
   return (
     <div>
-      <p style={{ margin: '0' }}>good {good}</p>
-      <p style={{ margin: '0' }}>neutral {neutral}</p>
-      <p style={{ margin: '0' }}>bad {bad}</p>
+      <p> good {good} </p>
+      <p> neutral {neutral} </p>
+      <p> bad {bad} </p>
+	  <p> all {all} </p>
+	  <p> average {average} </p>
+	  <p> positive {positive}% </p>
     </div>
   )
 }
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
   return (
     <div>
       <Header title="give feedback"/>
-      <button onClick={() => {setGood(good + 1)}}>{"good"}</button>
-      <button onClick={() => {setNeutral(neutral + 1)}}>{"neutral"}</button>
-      <button onClick={() => {setBad(bad + 1)}}>{"bad"}</button>
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
       <Header title="statistics" />
       <FeedbackRes good={good} neutral={neutral} bad={bad}/>
     </div>
