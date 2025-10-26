219781a183fe00e05df134de32df39beb81f6dd5
Julia Llama <julia.persidskaia@gmail.com>
2025-10-26 12:17:52 +0200

    Update dependencies

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,67 +1,67 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
     </div>
-  )
-}
+  );
+};
 
 const StatisticLine = (props) => {
-	return (
-		<tr>
-			<td>{props.text}</td>
-			<td>{props.value}</td>
-		</tr>
-	)
-}
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+};
 
 const Statistics = (props) => {
   // console.log("Counter:", {good, neutral, bad})
-  const all = props.good + props.neutral + props.bad
+  const all = props.good + props.neutral + props.bad;
   if (all == 0) {
-	return (<div> No feedback given </div>)
+    return <div> No feedback given </div>;
   }
-  const average = (props.good - props.bad) / all
-  const positive = props.good / all * 100
+  const average = (props.good - props.bad) / all;
+  const positive = (props.good / all) * 100;
   return (
     <div>
-		<table>
-			<tbody>
-				<StatisticLine text="good" value={props.good} />
-      			<StatisticLine text="neutral" value={props.neutral} />
-      			<StatisticLine text="bad" value={props.bad} />
-	  			<StatisticLine text="all" value={all} />
-	  			<StatisticLine text="average" value={average} />
-	  			<StatisticLine text="positive" value={positive + '%'} />
-			</tbody>
-		</table>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + "%"} />
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
 
   return (
     <div>
-      <Header title="give feedback"/>
+      <Header title="give feedback" />
       <Button onClick={handleGoodClick} text="good" />
       <Button onClick={handleNeutralClick} text="neutral" />
       <Button onClick={handleBadClick} text="bad" />
       <Header title="statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
